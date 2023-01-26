43bf7047b5f434b47b96e2132fc6e97dfc595328
Jannik Streek <jannik.streek@gmail.com>
2023-01-26 10:51:43 +0100

    add basic auth and correct german prefix

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import nltk
 import cherrypy
+import os
 from HanTa import HanoverTagger as ht
 
 nltk.download("punkt")
@@ -15,7 +16,7 @@ def convert_tag_to_obj(tag_array):
 
 class NLPWordTagger:
   def __init__(self, lang="en"):
-    hanta_model = "morphmodel_ger.pgz" if lang == "ger" else "morphmodel_en.pgz"
+    hanta_model = "morphmodel_ger.pgz" if lang == "de" else "morphmodel_en.pgz"
     self.tagger = ht.HanoverTagger(hanta_model)
 
   def tag_words_from_string(self, string):
@@ -33,7 +34,19 @@ class NLPWordTaggerServer(object):
         words = tagger.tag_words_from_string(data)
         return words
 
-cherrypy.config.update({'server.socket_host': '0.0.0.0',
-                        'server.socket_port': 8080,
-                       })
-cherrypy.quickstart(NLPWordTaggerServer())
+userpassdict = {os.environ.get('BASIC_AUTH_USER_NAME') or 'admin': os.environ.get('BASIC_AUTH_USER_PASSWORD') or 'admin'}
+
+conf = {
+   '/': {
+       'tools.auth_basic.on': os.environ.get('BASIC_AUTH_ENABLED') or True,
+       'tools.auth_basic.realm': os.environ.get('SERVER_HOST') or '0.0.0.0',
+       'tools.auth_basic.checkpassword': cherrypy.lib.auth_basic.checkpassword_dict(userpassdict),
+       'tools.auth_digest.get_ha1': cherrypy.lib.auth_digest.get_ha1_dict_plain(userpassdict),
+       'tools.auth_basic.accept_charset': 'UTF-8',
+    },
+    'global': {
+      'server.socket_host': os.environ.get('SERVER_HOST') or '0.0.0.0',
+      'server.socket_port': int(os.environ.get('SERVER_PORT')) if os.environ.get('SERVER_PORT') else 8080
+    }
+}
+cherrypy.quickstart(NLPWordTaggerServer(), '/', config=conf)
